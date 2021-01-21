fa07fb72635a09162d838b3bb983f9f4e2684ce8
Amine-Aku <codenameaku@gmail.com>
2021-01-21 23:51:07 +0100

    Login Activity

---
+++ app/src/main/java/com/impression/ensapayagent/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.impression.ensapayagent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+    }
+
+    fun login(view: View) {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
